fe36a1a44741e839b63b4b36857a0f57dec3b309
Jan-Willem Harmannij <jwharmannij@gmail.com>
2023-07-26 22:46:27 +0200

    Improve notepad example

---
+++ Notepad/src/main/java/io/github/jwharm/javagi/examples/notepad/EditorWindow.java
@@ -0,0 +1,236 @@
+package io.github.jwharm.javagi.examples.notepad;
+
+import io.github.jwharm.javagi.annotations.InstanceInit;
+import io.github.jwharm.javagi.base.GErrorException;
+import io.github.jwharm.javagi.base.Out;
+import io.github.jwharm.javagi.gtk.types.Types;
+import org.gnome.gio.File;
+import org.gnome.gio.FileCreateFlags;
+import org.gnome.glib.Type;
+import org.gnome.gobject.GObject;
+import org.gnome.gtk.*;
+
+import java.lang.foreign.MemorySegment;
+
+/**
+ * The EditorWindow class contains a headerbar and the textview. The headerbar
+ * contains buttons for new/open/save actions.
+ */
+public class EditorWindow extends ApplicationWindow {
+
+    // The currently open file (or null when no file is open)
+    private File file = null;
+
+    // The textview component
+    private TextView textview;
+
+    // Register the EditorWindow class
+    private static final Type gtype = Types.register(EditorWindow.class);
+
+    public static Type getType() {
+        return gtype;
+    }
+
+    // Constructor for a new EditorWindow
+    public static EditorWindow create(Application application) {
+        EditorWindow window = GObject.newInstance(getType());
+        window.setApplication(application);
+        return window;
+    }
+
+    // Memory-address constructor, should always be present
+    public EditorWindow(MemorySegment address) {
+        super(address);
+    }
+
+    /**
+     * The @InstanceInit method is called during construction of a new EditorWindow instance.
+     * It creates the window layout (the headerbar, buttons, and textview).
+     */
+    @InstanceInit
+    public void init() {
+
+        // Create the headerbar
+        var header = new HeaderBar();
+        super.setTitlebar(header);
+
+        // Create the TextView and set the font style to monospace
+        textview = new TextView();
+        textview.setMonospace(true);
+
+        // The window title contains a "modified" indicator, that is
+        // updated by the "on-modified-change" signal of the text buffer.
+        textview.getBuffer().onModifiedChanged(this::updateWindowTitle);
+
+        // The textView should be scrollable.
+        var scrolledWindow = ScrolledWindow.builder()
+                .child(textview)
+                .vexpand(true)
+                .build();
+        super.setChild(scrolledWindow);
+
+        // Create buttons for 'new', 'open' and 'save' actions.
+        var newButton = Button.newFromIconName("document-new-symbolic");
+        newButton.onClicked(() -> whenSure(this::clear));
+        header.packStart(newButton);
+
+        var openButton = Button.newFromIconName("document-open-symbolic");
+        openButton.onClicked(() -> whenSure(this::open));
+        header.packStart(openButton);
+
+        var saveButton = Button.newFromIconName("document-save-symbolic");
+        saveButton.onClicked(this::save);
+        header.packStart(saveButton);
+
+        // Ask to save changes before closing the window.
+        this.onCloseRequest(() -> {
+            whenSure(this::destroy);
+            return true;
+        });
+
+        // Show the results.
+        updateWindowTitle();
+        super.present();
+
+        // Make sure the text field has the keyboard focus.
+        textview.grabFocus();
+    }
+
+    /**
+     * Updates the window title to a modified-indicator and the current filename.
+     * When no file is open, the title is "Unnamed".
+     */
+    private void updateWindowTitle() {
+        super.setTitle(
+                (textview.getBuffer().getModified() ? ("• ") : "") +
+                (file == null ? "Unnamed" : file.getBasename())
+        );
+    }
+
+    /**
+     * Runs {@code action} but, if the buffer is modified, asks to save
+     * the modifications first.
+     *
+     * @param action the action to run after saving the modifications
+     */
+    private void whenSure(Runnable action) {
+        // No modifications?
+        if (! textview.getBuffer().getModified()) {
+            action.run();
+            return;
+        }
+
+        // Setup the confirmation dialog with three buttons:
+        // 0 - Cancel, 1 - Discard, 2 - Save
+        AlertDialog alert = AlertDialog.builder()
+                .modal(true)
+                .message("Save changes?")
+                .detail("Do you want to save your changes?")
+                .buttons(new String[] {"Cancel", "Discard", "Save"})
+                .cancelButton(0)
+                .defaultButton(2)
+                .build();
+
+        // Get dialog result
+        alert.choose(this, null, (object, result, data) -> {
+            try {
+                int button = alert.chooseFinish(result);
+                if (button == 0) return; // cancel
+                if (button == 2) save(); // save
+                action.run();
+            } catch (GErrorException ignored) {} // user clicked cancel
+        });
+    }
+
+    /**
+     * "New" action: clear the editor buffer.
+     * This could also open a new window, instead of cleaning the current buffer.
+     */
+    public void clear() {
+        file = null;
+    	textview.getBuffer().setText(" ", 0);
+    	textview.getBuffer().setModified(false);
+        textview.grabFocus();
+    }
+
+    /**
+     * "Open" action: Load a file and show the contents in the editor.
+     */
+    public void open() {
+        // Setup an Open File dialog.
+        var dialog = new FileDialog();
+        dialog.open(this, null, (object, result, data) -> {
+            try {
+                file = dialog.openFinish(result);
+            } catch (GErrorException ignored) {} // used clicked cancel
+            if (file == null) return;
+
+            // Load the contents of the selected file.
+            try {
+                // The byte[] parameter is an out-parameter in the C API.
+                // Create an empty Out<byte[]> object, and read its value afterwards.
+                Out<byte[]> contents = new Out<>();
+                file.loadContents(null, contents, null);
+                textview.getBuffer().setText(new String(contents.get()), contents.get().length);
+                textview.getBuffer().setModified(false);
+                textview.grabFocus();
+            } catch (GErrorException e) {
+                AlertDialog.builder()
+                        .modal(true)
+                        .message("Error reading from file")
+                        .detail(e.getMessage())
+                        .build()
+                        .show(this);
+            }
+        });
+    }
+
+    /**
+     * "Save" action: Show a file dialog (for new files) and call {@link #write()}
+     */
+    public void save() {
+        if (file == null) {
+            // Setup a Save File dialog.
+            var dialog = new FileDialog();
+            dialog.save(this, null, (object, result, data) -> {
+                try {
+                    file = dialog.saveFinish(result);
+                    if (file == null) return;
+
+                    // Write the textview buffer contents to the selected file.
+                    write();
+                    textview.getBuffer().setModified(false);
+                    textview.grabFocus();
+                } catch (GErrorException ignored) {} // used clicked cancel
+            });
+        } else {
+            // Write the textview buffer contents to the file that was already open.
+            write();
+            textview.getBuffer().setModified(false);
+            textview.grabFocus();
+        }
+    }
+
+    /**
+     * Helper function that writes editor contents to a file.
+     */
+    private void write() {
+        // Get the contents of the textview buffer as a byte array
+    	TextIter start = TextIter.allocate();
+    	TextIter end = TextIter.allocate();
+    	textview.getBuffer().getBounds(start, end);
+    	byte[] contents = textview.getBuffer().getText(start, end, false).getBytes();
+
+        // Write the byte array to the file
+    	try {
+			file.replaceContents(contents, "", false, FileCreateFlags.NONE, null, null);
+		} catch (GErrorException e) {
+            AlertDialog.builder()
+                    .modal(true)
+                    .message("Error writing to file")
+                    .detail(e.getMessage())
+                    .build()
+                    .show(this);
+        }
+    }
+}
--- Notepad/src/main/java/io/github/jwharm/javagi/examples/notepad/Notepad.java
+++ Notepad/src/main/java/io/github/jwharm/javagi/examples/notepad/Notepad.java
@@ -1,196 +1,54 @@
 package io.github.jwharm.javagi.examples.notepad;
 
-import org.gnome.adw.Application;
-import org.gnome.adw.ApplicationWindow;
-import org.gnome.adw.HeaderBar;
+import io.github.jwharm.javagi.gtk.types.Types;
 import org.gnome.gio.ApplicationFlags;
-import org.gnome.gio.File;
-import org.gnome.gio.FileCreateFlags;
-import org.gnome.gtk.*;
+import org.gnome.glib.Type;
+import org.gnome.gobject.GObject;
+import org.gnome.gtk.Application;
 
-import io.github.jwharm.javagi.base.GErrorException;
-import io.github.jwharm.javagi.base.Out;
+import java.lang.foreign.MemorySegment;
 
+/**
+ * This is the Notepad application class. It contains the main method that will
+ * create and run the application.
+ */
 public class Notepad extends Application {
 
-	// Main method: runs the application
     public static void main(String[] args) {
-        var app = new Notepad("io.github.jwharm.samples.Notepad", ApplicationFlags.NON_UNIQUE);
-        app.onActivate(app::activate);
+        var app = Notepad.create();
         app.run(args);
     }
-    
-    // The application main window
-    ApplicationWindow window;
-    
-    // The text editor widget
-    TextView textView;
-    
-    // File handle of an opened file
-    File file;
-    
-    // Indicates whether the open document has been modified
-    boolean modified = false;
-    
-    // Window title (file name or "Unnamed")
-    String title = "Unnamed";
-    
-    public Notepad(String applicationId, ApplicationFlags flags) {
-        super(applicationId, flags);
+
+    // Register the Notepad class
+    private static final Type gtype = Types.register(Notepad.class);
+
+    public static Type getType() {
+        return gtype;
     }
-    
+
+    // Constructor
+    public static Notepad create() {
+        Notepad app = GObject.newInstance(getType());
+        app.setApplicationId("io.github.jwharm.javagi.examples.Notepad");
+        app.setFlags(ApplicationFlags.DEFAULT_FLAGS);
+        return app;
+    }
+
+    // Memory-address constructor, should always be present
+    public Notepad(MemorySegment address) {
+        super(address);
+    }
+
+    /**
+     * When the application is activated, create a new EditorWindow
+     */
+    @Override
     public void activate() {
-    	// Create window
-        window = new ApplicationWindow(this);
-        window.setDefaultSize(600, 400);
-        window.setTitle(title);
-        
-        // Create layout box
-        var vbox = new Box(Orientation.VERTICAL, 0);
-        
-        // Create header bar
-        var headerBar = new HeaderBar();
-        vbox.append(headerBar);
-        
-        // Create the editor widget and set the font style to monospace
-        textView = new TextView();
-        textView.setMonospace(true);
-
-        // The text editor should be scrollable
-        var scrolledWindow = new ScrolledWindow();
-        scrolledWindow.setChild(textView);
-        scrolledWindow.setVexpand(true);
-        vbox.append(scrolledWindow);
-        
-        // Create buttons for 'new', 'open' and 'save' actions.
-        // The builder pattern is used to construct the objects.
-        // Each button gets an icon (from the default icons included in GTK),
-        // and the 'clicked' signal is connected to a method in this class.
-        
-        Button newButton = Button.builder()
-                .label("New...")
-                .iconName("document-new-symbolic")
-                .build();
-        newButton.onClicked(this::clear);
-        headerBar.packStart(newButton);
-        
-        Button openButton = Button.builder()
-                .label("Open...")
-                .iconName("document-open-symbolic")
-                .build();
-        openButton.onClicked(this::open);
-        headerBar.packStart(openButton);
-        
-        Button saveButton = Button.builder()
-                .label("Save")
-                .iconName("document-save-symbolic")
-                .build();
-        saveButton.onClicked(this::save);
-        headerBar.packEnd(saveButton);
-        
-        // Add the layout box to the window and show the results
-        window.setContent(vbox);
-        window.present();
-        
-        // When the 'modified-changed' signal is emitted, update the window title
-        textView.getBuffer().onModifiedChanged(() -> {
-        	modified = textView.getBuffer().getModified();
-        	updateWindowTitle();
-        });
-        
-        // Make sure the text field has the keyboard focus
-        textView.grabFocus();
-    }
-    
-    // Use UTF 2022 (BULLET) character to indicate that the text has been modified
-    private void updateWindowTitle() {
-    	window.setTitle((modified ? ("• ") : "") + title);
-    }
-    
-    // New unnamed file
-    public void clear() {
-        file = null;
-    	title = "Unnamed";
-    	textView.getBuffer().setText("", 0);
-    	textView.getBuffer().setModified(false);
-    	modified = false;
-    	updateWindowTitle();
-    	textView.grabFocus();
-    }
-    
-    // Open a file (using FileChooserNative)
-    public void open() {
-
-        var dialog = new FileDialog();
-        dialog.open(window, null, (obj, res, d) -> {
-            try {
-                file = dialog.openFinish(res);
-                if (file == null) {
-                    return;
-                }
-
-                // Read file and put contents in the textview's text buffer. The read is done asynchronously.
-                file.loadContentsAsync(null, (object, result, data) -> {
-                    try {
-                        // The 'contents' and 'length' parameters are out-parameters. Because Java always does
-                        // pass-by-value, we need to wrap these into Out<> objects.
-                        Out<byte[]> contents = new Out<>();
-                        if (file.loadContentsFinish(result, contents, null)) {
-                            title = file.getBasename();
-                            updateWindowTitle();
-                            textView.getBuffer().setText(new String(contents.get()), contents.get().length);
-                            textView.grabFocus();
-                        }
-                    } catch (GErrorException e) {
-                        System.err.println("Error: " + e.getMessage());
-                    }
-                });
-            } catch (GErrorException ignored) {
-            }
-        });
-    }
-    
-    // Save a file. If the filename isn't known yet, show a 'Save file' dialog.
-    public void save() {
-    	if (file == null) {
-            var dialog = new FileDialog();
-            dialog.save(window, null, (obj, res, d) -> {
-                try {
-                    file = dialog.saveFinish(res);
-                    if (file == null) {
-                        return;
-                    }
-
-                    title = file.getBasename();
-                    textView.getBuffer().setModified(false);
-                    modified = false;
-                    updateWindowTitle();
-                    textView.grabFocus();
-                    write();
-                } catch (GErrorException ignored) {
-                }
-            });
-    	} else {
-    		// Reset the 'modified' indicator and update the window title
-	    	textView.getBuffer().setModified(false);
-	    	modified = false;
-			updateWindowTitle();
-	    	textView.grabFocus();
-    		write();
-    	}
-    }
-
-    // This method is used from save() to write the contents to the file.
-    private void write() {
-    	TextIter start = TextIter.allocate();
-    	TextIter end = TextIter.allocate();
-    	textView.getBuffer().getBounds(start, end);
-    	byte[] contents = textView.getBuffer().getText(start, end, false).getBytes();
-    	try {
-    		// This would better be executed asynchronously with File.replaceContentsAsync().
-			file.replaceContents(contents, "", false, FileCreateFlags.NONE, null, null);
-		} catch (GErrorException e) {
-            System.err.println("Error: " + e.getMessage());
+        var win = this.getActiveWindow();
+        if (win == null) {
+            win = EditorWindow.create(this);
         }
+        win.setDefaultSize(600, 400);
+        win.present();
     }
 }
