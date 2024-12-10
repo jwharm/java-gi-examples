8d43ecd9de3411d3c16dbdab9d73d048cb2001e1
poach3r <n0nam3b01@gmail.com>
2024-12-10 14:43:25 +0000

    Update java-gi version.

--- CalculatorKotlin/build.gradle.kts
+++ CalculatorKotlin/build.gradle.kts
@@ -7,15 +7,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven {
-        url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
-    }
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("io.github.jwharm.javagi:gtk:0.11.1-SNAPSHOT")
-    implementation("io.github.jwharm.javagi:adw:0.11.1-SNAPSHOT")
+    implementation("io.github.jwharm.javagi:gtk:0.11.1")
+    implementation("io.github.jwharm.javagi:adw:0.11.1)
 }
 
 kotlin {
